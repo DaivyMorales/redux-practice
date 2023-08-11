9e29d170163894a5ca3ef2f8b060b2102b3bd7cd
Daivy Morales <daivymorales7705@gmail.com>
2023-08-11 08:11:41 -0500

    feat: state name utility

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { useAppDispach, useAppSelector } from "@/redux/hooks";
 import { increment, decrement } from "@/redux/features/counterSlice";
+import { useState } from "react";
 
 export default function HomePage() {
   const count = useAppSelector((state) => state.counterReducer.counter);
   const dispatch = useAppDispach();
 
+  const [name, setName] = useState("Daivy");
+
   return (
     <div>
       <h1>Total: {count}</h1>
+      <h2>{name}</h2>
 
       <button
         onClick={() => {
@@ -24,7 +28,6 @@ export default function HomePage() {
       >
         Decrement
       </button>
-      
     </div>
   );
 }
